8c5fd545938675a3456af15163f06dad499f7aa6
Salvador González <zyrian.nova@gmail.com>
2024-05-18 13:53:26 -0700

    Añadida actualización de script de generación de contraseñas

--- passwords/password-generator-es.sh
+++ passwords/password-generator-es.sh
@@ -5,8 +5,15 @@
 # o credenciales seguras para un primer inicio de sesión.
 # Omití símbolos como '^' y '$' para evitar problemas con el script o simbolos que no se encuentren en teclados de forma sencilla.
 
-# Longitud de la contraseña (ajusta el número según tus necesidades)
-length=12
+# Define colores
+NC='\033[0m' # No Color
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+
+# Longitud de la contraseña por defecto
+default_length=10
 
 # Número mínimo de símbolos especiales requeridos
 min_symbols=3
@@ -16,6 +23,7 @@ characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%&*
 
 # Función para generar la contraseña
 generate_password() {
+  local length=$1
   local password=""
   local prev_char=""
   local consecutive_letters=0
@@ -39,8 +47,11 @@ generate_password() {
   echo "$password"
 }
 
+# Longitud de la contraseña proporcionada como parámetro, o usar el valor por defecto
+length=${1:-$default_length}
+
 # Generar la contraseña aleatoria
-password=$(generate_password)
+password=$(generate_password $length)
 
 # Mostrar la contraseña generada
-echo "Contraseña Generada: $password"
+echo -e "${BLUE}Contraseña Generada: ${YELLOW}$password${NC}"
